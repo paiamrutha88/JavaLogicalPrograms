ed05d0c0f27a8ae4ca7ba8c824b6afb667ccebde
Amrutha Pai <amrutha.pai@ness.com>
2019-10-19 09:46:52 +0530

    latest

--- src/com/intv/logi/CountEvenOdd.java
+++ src/com/intv/logi/CountEvenOdd.java
@@ -4,7 +4,29 @@ public class CountEvenOdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int num=1360;
+		int countEven=0;
+		int countOdd=0;
+		int mod=0;
+		
+		while(num>0)
+		{
+			mod=num%10;
+			
+			if(mod/2==0)
+			{
+				countEven++;
+			}
+			else
+			{
+				countOdd++;
+			}
+			num=num/10;
+		}
 
+		System.out.println("Even Numbers : "+countEven);
+		System.out.println("Count Odd : "+countOdd);
 	}
 
 }
