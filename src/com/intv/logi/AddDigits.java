7dd67cdeb66fecc9e103b87d0b1d9d65d46df9b4
Amrutha Pai <amrutha.pai@ness.com>
2019-10-19 09:49:51 +0530

    adding single file

---
+++ src/com/intv/logi/AddDigits.java
@@ -0,0 +1,20 @@
+package com.intv.logi;
+
+public class AddDigits {
+
+	public static void main(String[] args) {
+		
+		int num=121;
+		int sum=0;
+		
+		while(num>0)
+		{
+			sum=sum+num%10;
+			num=num/10;
+		}
+		
+		System.out.println("Sum of Digits : " +sum);
+
+	}
+
+}
