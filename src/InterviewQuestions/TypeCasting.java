4d25262a68ff38e27d046e891635f3f2000ed2f9
Amrutha Pai <amrutha.pai@ness.com>
2019-12-03 09:50:07 +0530

    reading coolies file

--- src/InterviewQuestions/TypeCasting.java
+++ src/InterviewQuestions/TypeCasting.java
@@ -3,6 +3,7 @@ package InterviewQuestions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class TypeCasting {
 	public static WebDriver driver;
